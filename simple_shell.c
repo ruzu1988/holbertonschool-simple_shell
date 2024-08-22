3df2e0ea7f276c59602ede971b507d0f3af6f239
edison0341 <edison0341@gmail.com>
2024-08-22 13:53:55 -0400

    fixed betty

--- simple_shell.c
+++ simple_shell.c
@@ -6,43 +6,43 @@
  */
 int main(void)
 {
-        char **args;
-        char *input = NULL;
-        size_t buffer_size = 0;
-        int count = 0, mode = isatty(STDIN_FILENO), comparator;
+	char **args;
+	char *input = NULL;
+	size_t buffer_size = 0;
+	int count = 0, mode = isatty(STDIN_FILENO), comparator;
 
-        while (1)
-        {
-                if (mode)
-                        printf("#cisfun$ ");
-                if (getline(&input, &buffer_size, stdin) == -1) /* Reading the user input */
-                {
-                        free(input);
-                        break; /* Handling the eof (ctrl + D) */
-                }
-                count++;
-                if (input[strlen(input)] == '\n') /* Trim trailing the newline */
-                        input[strlen(input)] = '\0';
-                args = parse_input(input); /* Tokenize the user input */
+	while (1)
+	{
+		if (mode)
+			printf("#cisfun$ ");
+		if (getline(&input, &buffer_size, stdin) == -1) /* Reading the user input */
+		{
+			free(input);
+			break; /* Handling the eof (ctrl + D) */
+		}
+		count++;
+		if (input[strlen(input)] == '\n') /* Trim trailing the newline */
+			input[strlen(input)] = '\0';
+		args = parse_input(input); /* Tokenize the user input */
 
-                if (args == NULL)
-                {
-                        free(input);
-                        free(args);
-                        continue;
-                }
-                if (args[0] != NULL)
-                {
-                        comparator = env_fetch(args, input, count);
-                        free(args);
-                        if (comparator == 0)
-                                continue;
-                        else
-                                break;
-                }
-                free(args);
-        }
-        if (mode)
-                printf("\n");
-        return (0);
+		if (args == NULL)
+		{
+			free(input);
+			free(args);
+			continue;
+		}
+		if (args[0] != NULL)
+		{
+			comparator = env_fetch(args, input, count);
+			free(args);
+			if (comparator == 0)
+				continue;
+			else
+				break;
+		}
+		free(args);
+	}
+	if (mode)
+		printf("\n");
+	return (0);
 }
